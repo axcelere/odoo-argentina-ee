b4eb07d5ddb14caab391f203c1fcc475f0dad50d
raguero </SUlem@2017/>
2021-10-10 15:50:52 -0300

    [REM][l10n_ar_account_reports]

--- l10n_ar_account_reports/models/__init__.py
+++
@@ -1,7 +0,0 @@
-##############################################################################
-# For copyright and license notices, see __manifest__.py file in module root
-# directory
-##############################################################################
-from . import account_report
-from . import account_journal
-from . import account_generic_tax_report
